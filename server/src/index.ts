62d5324917946f73d5248dd267b48ce628842ae2
Beatso <39217836+Beatso@users.noreply.github.com>
2021-06-05 10:30:29 +0100

    backend setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello world')
+// imports
+import { config } from 'dotenv'
+import { Dropbox } from 'dropbox'
+import express from 'express'
+
+// dotenv setup
+config()
+
+// dropbox setup
+const dbx = new Dropbox({
+	accessToken: process.env.DROPBOXTOKEN,
+})
+
+// express setup
+const app = express()
+app.use(express.json()) // to support JSON-encoded bodies
+app.get('/ping', (req, res) => res.send('pong'))
+
+// ...
+
+// listen express server
+const port = process.env.PORT || 3100
+app.listen(port, () => console.log(`Server running on port ${port}`))
